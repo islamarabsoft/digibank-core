ca21784c7523705456ebcea45b25909107d3e409
MH <mohamed.hanafy@live.com>
2023-02-22 14:27:10 +0200

    init security configuration in gateway

--- core/core-application/src/main/java/com/segmaware/core/controller/Activity.java
+++ core/core-application/src/main/java/com/segmaware/core/controller/Activity.java
@@ -2,9 +2,12 @@ package com.segmaware.core.controller;
 
 import com.segmaware.utility.api.response.RestResponseEntity;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @Slf4j
 @RestController
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Activity {
     @RestResponseEntity
     @GetMapping
+    @RolesAllowed({"activity"})
     public String createActivity(){
         log.info("Execute Function");
         return "Activity Message Display";
--- gateway/src/main/java/com/segmaware/digibank/GatewayRunner.java
+++ gateway/src/main/java/com/segmaware/digibank/GatewayRunner.java
@@ -1,12 +1,10 @@
 package com.segmaware.digibank;
 
-import com.segmaware.digibank.config.GatewayConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(GatewayConfiguration.class)
 public class GatewayRunner {
     public static void main(String[] args) {
         SpringApplication.run(GatewayRunner.class, args);
